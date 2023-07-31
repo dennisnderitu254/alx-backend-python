b7037ecdd01e63e76d92eb7b52782be2c52985ac
dennisnderitu254 <dknderitu@gmail.com>
2023-07-31 13:43:09 +0300

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -102,3 +102,10 @@ def requests_get(*args, **kwargs):
         return MockResponse(TEST_PAYLOAD[0][0])
     if args[0] == TEST_PAYLOAD[0][0]["repos_url"]:
         return MockResponse(TEST_PAYLOAD[0][1])
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    [(TEST_PAYLOAD[0][0], TEST_PAYLOAD[0][1], TEST_PAYLOAD[0][2],
+      TEST_PAYLOAD[0][3])]
+)
